6c0ced3c3687bb5fe8621263b3b89f52f787bf09
Bohdan Kolomiiets <kb907406@gmail.com>
2023-10-19 18:18:41 +0300

    Add ordering to models

--- kitchen/models.py
+++ kitchen/models.py
@@ -7,12 +7,19 @@ from restaurant_kitchen_service.settings import AUTH_USER_MODEL
 class DishType(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    class Meta:
+        ordering = ("name", )
+
     def __str__(self):
         return self.name
 
 
 class Cook(AbstractUser):
-    years_of_experience = models.PositiveIntegerField()
+    years_of_experience = models.PositiveIntegerField(blank=True, null=True)
+
+    class Meta:
+        ordering = ("username", )
+
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
 
@@ -30,6 +37,9 @@ class Dish(models.Model):
         to=AUTH_USER_MODEL,
         related_name="dishes"
     )
+
+    class Meta:
+        ordering = ("dish_type", )
 
     def __str__(self):
         return f"{self.name} (price: {self.price}, dish type: {self.dish_type.name}"
