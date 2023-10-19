c83f0c86f0c5d7967afab8097bdee3d888f0f677
Bohdan Kolomiiets <kb907406@gmail.com>
2023-10-19 17:46:00 +0300

    Setup DishType, Cook and Dish models, add AUTH_USER_MODEL to settings

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from restaurant_kitchen_service.settings import AUTH_USER_MODEL
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.PositiveIntegerField()
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(
+        to=DishType,
+        on_delete=models.CASCADE,
+        related_name="dishes"
+    )
+    cooks = models.ManyToManyField(
+        to=AUTH_USER_MODEL,
+        related_name="dishes"
+    )
+
+    def __str__(self):
+        return f"{self.name} (price: {self.price}, dish type: {self.dish_type.name}"
