087750c0c9f1964ae31f5f43de502baaa7030d3c
Bohdan Kolomiiets <kb907406@gmail.com>
2023-10-20 18:32:14 +0300

    Add new view and simple html page for Dish Type model

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import (
     index,
+    DishTypeListView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
+    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list")
 ]
 
 app_name = "kitchen"
