45101db2321941d1bfa9ab1329586a0fb2cd223f
Bohdan Kolomiiets <kb907406@gmail.com>
2023-10-20 15:24:08 +0300

    Add urls.py to kitchen app, add new index view and simple home page

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import (
+    Cook,
+    Dish,
+    DishType,
+    Ingredient
+)
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    """View function for the home page of the site."""
+    return render(request, "kitchen/index.html")
