43b964b684f412493aa0cc5b8341347865c36fb6
Bohdan Kolomiiets <kb907406@gmail.com>
2023-10-20 19:07:05 +0300

    Add new view and page for Dish model, also update sidebar

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from .views import (
     index,
     DishTypeListView,
+    DishListView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
-    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list")
+    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
+    path("dish/", DishListView.as_view(), name="dish-list")
 ]
 
 app_name = "kitchen"
